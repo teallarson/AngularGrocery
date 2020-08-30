d53e35e87e7b277d369e176ef400011fcc6face9
Teal Larson <larson.teal@gmail.com>
2020-08-29 20:42:56 -0700

    fiddled with alignment

--- src/app/grocery/app.grocery.ts
+++ src/app/grocery/app.grocery.ts
@@ -20,19 +20,36 @@ export class GroceryComponent {
     if(this.task.id == 0) {
     this.tasks.push({id: (new Date()).getTime(), name: this.task.name, strike: false});
     }
-    this.task.name = '';
-    this.task.id = 0;
-    }
+    this.task = {
+      name: '',
+      id: 0
+    };
+  }
     
     onEdit(item) {
     this.task = item;
     }
     
     onDelete(item) {
-    this.tasks = this.tasks.filter(i => i.id !== item.id)
+      for(var i = 0;i < this.tasks.length; i++){
+        if(item.id == this.tasks[i].id){
+          this.tasks.splice(i,1);
+          break;
+        }
+      }
     }
     
-    onStrike(item) {
-    this.tasks.filter(i => i.id === item.id)[0].strike = !this.tasks.filter(i => i.id === item.id)[0].strike;
+    onStrike(item){
+      for(var i = 0;i < this.tasks.length; i++){
+        if(item.id == this.tasks[i].id){
+          if(this.tasks[i].strike){
+            this.tasks[i].strike = false;
+          }
+          else{
+            this.tasks[i].strike = true;
+          }
+          break;
+        }
+      }
     }
-  }
+}
